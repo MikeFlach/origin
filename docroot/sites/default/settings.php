dc5b0118d1f9afad14538528fc9c43a60e69cb1d
Michael Le Du <mledu@maxim.com>
2011-10-20 15:54:21 -0400

    updated settings.php for default site

--- docroot/sites/default/settings.php
+++ docroot/sites/default/settings.php
@@ -24,8 +24,7 @@ ini_set('session.use_only_cookies', 1);
 ini_set('session.use_trans_sid',    0);
 ini_set('url_rewriter.tags',        '');
 
-
-switch ($_REQUEST['SERVER_NAME']) {
+switch ($_SERVER['SERVER_NAME']) {
   case 'www.maxim.com':
   case 'maxim.com':
   case 'maxim.prod.acquia-sites.com':
@@ -33,4 +32,7 @@ switch ($_REQUEST['SERVER_NAME']) {
   case 'maximdev.prod.acquia-sites.com':
     require('/var/www/site-php/maxim/maxim-settings.inc');
     break;
+  default:
+  	require('local.settings.php');
+  	break;
 }
