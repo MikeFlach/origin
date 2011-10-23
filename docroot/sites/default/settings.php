8664aa2a3ad9e2fa2693762fd6a9aff2f6361c46
Michael Le Du <mledu@maxim.com>
2011-10-23 15:07:14 -0400

    added site staging-1047.prod.hosting.acquia.com to settings.php

--- docroot/sites/default/settings.php
+++ docroot/sites/default/settings.php
@@ -32,6 +32,7 @@ switch ($_SERVER['SERVER_NAME']) {
   case 'maxim.prod.acquia-sites.com':
   case 'maximstg.prod.acquia-sites.com':
   case 'maximdev.prod.acquia-sites.com':
+  case 'staging-1047.prod.hosting.acquia.com':
     require('/var/www/site-php/maxim/maxim-settings.inc');
     break;
   default:
