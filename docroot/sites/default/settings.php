db4ef762443ec8523e0e26bba29c3e78e2f11e8c
Michael Le Du <mledu@maxim.com>
2011-10-23 04:34:46 -0400

    changed ['SERVER_NAME'] to ['HTTP_HOST'] in settings.php

--- docroot/sites/default/settings.php
+++ docroot/sites/default/settings.php
@@ -24,7 +24,9 @@ ini_set('session.use_only_cookies', 1);
 ini_set('session.use_trans_sid',    0);
 ini_set('url_rewriter.tags',        '');
 
-switch ($_SERVER['SERVER_NAME']) {
+//switch ($_SERVER['SERVER_NAME']) {
+
+switch ($_SERVER['HTTP_HOST']) {
   case 'www.maxim.com':
   case 'maxim.com':
 	case 'dev.maxim.com':
