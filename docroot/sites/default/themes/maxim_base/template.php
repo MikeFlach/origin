64abb65684063dda195a178ead4b125f9c1f85b5
Michael Le Du <mledu@maxim.com>
2011-11-29 20:46:57 -0500

    Added jquery-1.71 and overrode the default jquery in the theme

--- docroot/sites/default/themes/maxim_base/template.php
+++ docroot/sites/default/themes/maxim_base/template.php
@@ -5,8 +5,12 @@
  * This file is empty by default because the base theme chain (Alpha & Omega) provides
  * all the basic functionality. However, in case you wish to customize the output that Drupal
  * generates through Alpha & Omega this file is a good place to do so.
- * 
+ *
  * Alpha comes with a neat solution for keeping this file as clean as possible while the code
  * for your subtheme grows. Please read the README.txt in the /preprocess and /process subfolders
  * for more information on this topic.
  */
+
+function maxim_base_js_alter(&$javascript) {
+  $javascript['misc/jquery.js']['data'] = libraries_get_path('jquery')  . '/jquery-1.7.1.min.js';
+}
