44ca3e01026e4e0662d3eb0093bc025bcfe2942c
Bill <bbreen@maxim.com>
2011-12-05 21:40:47 -0500

    cleaned up video code a bit

--- docroot/sites/default/themes/maxim_base/templates/node--video.tpl.php
+++ docroot/sites/default/themes/maxim_base/templates/node--video.tpl.php
@@ -18,11 +18,8 @@
       // We hide the comments and links now so that we can render them later.
       hide($content['comments']);
       hide($content['links']);
-      //drupal_add_js('http://pshared.5min.com/Scripts/PlayerSeed.js?sid=764&amp;width=620&amp;height=348&amp;shuffle=0&amp;sequential=1&amp;autoStart=true&amp;playList=517179106,517179103,517179099,517179097,517179096,517179095,517179094,517179087,517179086,517179084','external');
-      //dpm($node->field_media_file['und']['0']['field_media_5min_id']['und']['0']['value']);
-      //print
+      //print 5min player script as well as the current node 5min ID value
       print '<script type="text/javascript" src="http://pshared.5min.com/Scripts/PlayerSeed.js?sid=764&amp;amp;width=620&amp;amp;height=348&amp;amp;shuffle=0&amp;amp;sequential=1&amp;amp;autoStart=true&amp;amp;playList=' . $node->field_media_file['und']['0']['field_media_5min_id']['und']['0']['value'] . '"></script>';
-    
       print render($content);
     ?>
   </div>
