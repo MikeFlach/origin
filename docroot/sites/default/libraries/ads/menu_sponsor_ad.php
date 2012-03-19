a52b96f366321f792800cb4585f0d516ba6edbd5
Bill Breen <bbreen@maxim.com>
2012-03-19 14:22:48 -0400

    added gamergirl ad to menu sponsor code

--- docroot/sites/default/libraries/ads/menu_sponsor_ad.php
+++ docroot/sites/default/libraries/ads/menu_sponsor_ad.php
@@ -51,14 +51,6 @@ break;
 
 /* if $surl is defined, then override top level ad */
 switch ($surl){
-  case '/entertainment/oscars':
-    $ad = <<<AD
-    <!-- begin ad tag -->
-    <script type="text/javascript" src="http://ad.doubleclick.net/adj/maxim.dart/;advertiser=ford;sz=145x40;ord=[timestamp]?"></script>
-    <noscript><a href="http://ad.doubleclick.net/jump/maxim.dart/;advertiser=ford;sz=145x40;ord=[timestamp]?"><img src="http://ad.doubleclick.net/ad/maxim.dart/;advertiser=ford;sz=145x40;ord=[timestamp]?" border="0" alt="" /></a></noscript>
-    <!-- end ad tag -->
-AD;
-  break;
   case '/girls/hometown-hotties':
     $ad = <<<AD
     <a href="http://ad.doubleclick.net/jump/maxim.dart/;adid=253396528;sz=145x40;ord=[timestamp]?" target="_blank"><img src="http://ad.doubleclick.net/ad/maxim.dart/;adid=253396528;sz=145x40;ord=[timestamp]?" border="0" alt="" /></a>
@@ -82,6 +74,19 @@ case '/springbreak':
     //]]>
     </script>
     <noscript><a href="http://ad.doubleclick.net/jump/maxim.dart/;adid=254469768;sz=145x40;ord=123456789?" target="_blank" ><img src="http://ad.doubleclick.net/ad/maxim.dart/;adid=254469768;sz=145x40;ord=123456789?" border="0" alt="" /></a></noscript>
+    <!-- end ad tag -->
+AD;
+  break;
+case '/gamergirl':
+    $ad = <<<AD
+    <!-- begin ad tag -->
+    <script type="text/javascript">
+    //<![CDATA[
+    ord=Math.random()*10000000000000000;
+    document.write('<script type="text/javascript" src="http://ad.doubleclick.net/adj/maxim.dart/;adid=254661241;sz=145x40;ord=' + ord + '?"><\/script>');
+    //]]>
+    </script>
+    <noscript><a href="http://ad.doubleclick.net/jump/maxim.dart/;adid=254661241;sz=145x40;ord=123456789?" target="_blank" ><img src="http://ad.doubleclick.net/ad/maxim.dart/;adid=254661241;sz=145x40;ord=123456789?" border="0" alt="" /></a></noscript>
     <!-- end ad tag -->
 AD;
   break;
