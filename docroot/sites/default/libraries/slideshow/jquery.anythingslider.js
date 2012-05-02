638871b2c5d35cb19cd2622769bec03dfd4e53c6
Henry Wan <hwan@maxim.com>
2012-05-02 10:03:48 -0400

    Slideshow JS - new GumGum change

--- docroot/sites/default/libraries/slideshow/jquery.anythingslider.js
+++ docroot/sites/default/libraries/slideshow/jquery.anythingslider.js
@@ -108,8 +108,6 @@ if(typeof console =='undefined'){
       // When autoplay isn't passed, we stop the timer
       bodyTxt = '';
       attributionTxt = '';
-      base.$items.css('visibility','hidden');
-      base.$items.eq(page).css('visibility','visible'); 
       if (base.attribution[page].length > 0) {
         attributionTxt += '<div class="attribution">' + base.attribution[page] + '</div>';
       }
@@ -226,6 +224,8 @@ if(typeof console =='undefined'){
 
       // Update local variable
       base.currentPage = page;
+      base.$items.css('visibility','hidden');
+      base.$items.eq(page).css('visibility','visible'); 
     };
 
     base.goForward = function(autoplay) {
