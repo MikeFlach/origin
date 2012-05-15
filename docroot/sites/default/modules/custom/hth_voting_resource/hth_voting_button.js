732ac66851302f218645c029cb187c7c559a1c9a
Michael Flach <mflach@maxim.com>
2012-05-15 19:41:13 -0400

    fixed resat api resource. POST cretaes vote. get GETS vote validation

--- docroot/sites/default/modules/custom/hth_voting_resource/hth_voting_button.js
+++ docroot/sites/default/modules/custom/hth_voting_resource/hth_voting_button.js
@@ -1,7 +1,16 @@
 (function($){ $(document).ready(function() {
   $('#hth_vote').click(function() {
     uuid = $.cookie('maxim_uuid');
-    $.ajax({ url: '/js-api/vote/'+Drupal.settings.Maxim.nid+':::::'+uuid+'.json', type: 'GET',
-      success: function() { alert('s'); },
-      error: function() { alert('f'); },cache:false});
-});});})(jQuery)
+    $.ajax({ url: '/js-api/vote',
+             type: 'POST',
+             data: {
+               field: 'data',
+               'nid' : Drupal.settings.Maxim.nid,
+               'uid':  uuid
+             },
+             success: function() { $('#hth_vote').after('<div id="vote-success"></div>');  $('#hth_vote').fadeOut('slow', function() { $('#vote-success').text('Thank You For Your Vote!'); }); },
+             error: function() { alert('f'); },cache:false});
+           });
+     });
+})(jQuery)
+
