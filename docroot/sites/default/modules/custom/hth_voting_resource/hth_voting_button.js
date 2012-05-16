f7fa84e8e18e45b1f0636f5d3a4950a04c905ee8
Michael Flach <mflach@maxim.com>
2012-05-16 12:51:43 -0400

    adding vote checking to set_uuid.php

--- docroot/sites/default/modules/custom/hth_voting_resource/hth_voting_button.js
+++ docroot/sites/default/modules/custom/hth_voting_resource/hth_voting_button.js
@@ -3,11 +3,7 @@
     uuid = $.cookie('maxim_uuid');
     $.ajax({ url: '/js-api/vote',
              type: 'POST',
-             data: {
-               field: 'data',
-               'nid' : Drupal.settings.Maxim.nid,
-               'uid':  uuid
-             },
+             data: {nid: Drupal.settings.Maxim.nid, uid: uuid},
              success: function() { $('#hth_vote').after('<div id="vote-success"></div>');  $('#hth_vote').fadeOut('slow', function() { $('#vote-success').text('Thank You For Your Vote!'); }); },
              error: function() { alert('f'); },cache:false});
            });
