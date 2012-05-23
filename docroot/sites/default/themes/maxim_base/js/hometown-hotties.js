c27dffba65006490c9e1f98cfa016b6b34874ecf
Henry Wan <hwan@maxim.com>
2012-05-23 15:20:10 -0400

    Hotties - move JS to one file

--- docroot/sites/default/themes/maxim_base/js/hometown-hotties.js
+++ docroot/sites/default/themes/maxim_base/js/hometown-hotties.js
@@ -40,7 +40,16 @@ function hottieSliderHighlight() {
   }
 }
 
+
 jQuery(function(){
+  if (typeof Drupal.settings.Maxim.hottie.hottie_prev != 'undefined' && Drupal.settings.Maxim.hottie.hottie_prev.length > 0){
+    jQuery(".field-name-hotties-previous-next .hottie-prev-btn").html('<a href="' + Drupal.settings.Maxim.hottie.hottie_prev + '"><img src="http://cdn2.maxim.com/maxim/sites/default/libraries/hth/hottie_prev.png" width="111" height="22" alt="Previous Hottie" title="Previous Hottie"  /></a>');
+    jQuery(".field-name-hotties-previous-next .hottie-prev-btn").show();
+  }
+  if (typeof Drupal.settings.Maxim.hottie.hottie_next != 'undefined' && Drupal.settings.Maxim.hottie.hottie_next.length > 0){
+    jQuery(".field-name-hotties-previous-next .hottie-next-btn").html('<a href="' + Drupal.settings.Maxim.hottie.hottie_next + '"><img src="http://cdn2.maxim.com/maxim/sites/default/libraries/hth/hottie_next.png" width="111" height="22" alt="Next Hottie" title="Next Hottie" /></a>');
+    jQuery(".field-name-hotties-previous-next .hottie-next-btn").show();
+  }
   Drupal.settings.Maxim.hottie.curr_slider_page = 0;
   hottieSliderGoTo();
 });
