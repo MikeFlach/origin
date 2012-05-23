44d00821220dfa1f1ef9a7d4009487241a08ebc3
Henry Wan <hwan@maxim.com>
2012-05-23 14:08:43 -0400

    Hotties - Prev/Next buttons, Remove extra maxim_fields dir under maxim_fields

--- docroot/sites/default/modules/custom/hth_voting_resource/hth_voting_button.js
+++ docroot/sites/default/modules/custom/hth_voting_resource/hth_voting_button.js
@@ -5,14 +5,10 @@
              type: 'POST',
              data: {nid: Drupal.settings.Maxim.nid, uid: uuid},
              success: function(data) {
-                //alert(data);
-                //for some reason dashes are being appended to the result.
                 if (data.indexOf('vote_entered') != -1) {
-                   $('#hth_vote').after('<div id="vote-success"></div>');
                    $('#hth_vote').fadeOut('slow', function() {
+                     $('#hth_vote').after('<div id="vote-success"></div>');
                      $('#vote-success').text('Thank You For Your Vote!');
-                    // $('#vote-success').after('<div style="display: none;"><div id="social_data" style="padding: 10px; background: none repeat scroll 0% 0% rgb(255, 255, 255);">ROCK STAR</div></div><a id="social_pop" class="inline cboxElement" href="#inline_content"></a>');
-                    // $("#social_pop").click();
                    });
                  }
               },
