05bd9f35ca7f2966786543ea390e8cf957b51a76
Bill Breen <bbreen@maxim.com>
2012-07-31 17:19:22 -0400

    enabled issuu interstitial for hth sip

--- docroot/sites/all/modules/custom/interstitial_float/interstitial.js
+++ docroot/sites/all/modules/custom/interstitial_float/interstitial.js
@@ -1,10 +1,10 @@
-var interstitial = new ( function($) { 
+var interstitial = new ( function($) {
 	var me = this;
 	var intCircCookieExp = 1; //interstitial circ cookie exp (days)
 	var intAnimDelay = 2000; //milliseconds for animation
 	var windowTimeout;
-	var secsLeft = 30; //seconds before window closes
-	
+	var secsLeft = 60; //seconds before window closes
+
 	$(function() {
     /* Only display for larger screens */
     if($(window).width() < 980){
@@ -13,29 +13,29 @@ var interstitial = new ( function($) {
 
 		/* close event for the modal (interstitial circ) */
 		$(".modal .close").bind("click", function() {
-			$(this).parents(".modal").fadeOut(150);
+			$(this).parents(".modal").fadeOut(300);
 			return false;
 		});
-		
+
 		/* for testing - display interstitial */
 		//if(window.location.hash == "#interstitial") {
 		$(window).load(function() {
 			if(!$.cookie('mxm_intcirc')) {
 				$("#interstitialCirc").css({"display":"block","visibility":"hidden"});
-				
+
 				me.positionModal($("#interstitialCirc"));
 				$.cookie('mxm_intcirc', true, { expires:intCircCookieExp, path:'/'});
 			}
 		});
 		//}
-	}); 
-	
+	});
+
 	/* positions the modal in the middle of the page */
 	me.positionModal = function(el) {
 		if(!el){
 			el = ".modal";
 		}
-		
+
     modalW = $(el).outerWidth();
 		modalH = $(el).outerHeight();
 		$(el).css({"left" : -1 * $(el).outerWidth(), "top": ($(window).height()/2) - (modalH/2), "visibility":"visible" });
@@ -46,10 +46,10 @@ var interstitial = new ( function($) {
 				}
 				else {
 					clearInterval(windowTimeout);
-					$(el).fadeOut(150);
+					$(el).fadeOut(300);
 				}
 			}, 1000);
 		});
 	};
-	
+
 })(jQuery);
