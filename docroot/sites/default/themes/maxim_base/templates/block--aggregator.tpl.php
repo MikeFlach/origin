a524c20ab6662902a0228631406d8a8b9e95aee9
Bill Breen <bbreen@maxim.com>
2012-08-21 17:39:58 -0400

    added aggregator to maxim_core with install hook

--- docroot/sites/default/themes/maxim_base/templates/block--aggregator.tpl.php
+++ docroot/sites/default/themes/maxim_base/templates/block--aggregator.tpl.php
@@ -4,13 +4,13 @@
     <?php print render($title_prefix); ?>
       <?php if ($block->subject): ?>
         <?php switch ($block->subject) {
-          case 'TMZ (b)':
+          case 'TMZ':
             $partnerimg='http://cdn2.maxim.com/maxim/sites/default/files/TMZ_25px_0.png';
             break;
-          case 'Videobash (b)':
+          case 'Videobash':
             $partnerimg='http://cdn2.maxim.com/maxim/sites/default/files/logo_videobash_0.png';
             break;
-          case 'heavy (b)':
+          case 'Heavy':
             $partnerimg='http://cdn2.maxim.com/maxim/sites/default/files/logo_heavy_0.png';
             break;
           default:
