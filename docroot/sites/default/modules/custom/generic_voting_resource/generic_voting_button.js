480fa37457b8dedec5a2cd4e3bfe82a2494a7d6c
Michael Flach <mflach@maxim.com>
2012-11-06 19:57:14 -0500

    css/js/gallery updates for vody fortress

--- docroot/sites/default/modules/custom/generic_voting_resource/generic_voting_button.js
+++ docroot/sites/default/modules/custom/generic_voting_resource/generic_voting_button.js
@@ -16,7 +16,9 @@
               if (data.indexOf('vote_entered') != -1) {
                  $('#generic_vote').fadeOut('slow', function() {
                    $('#generic_vote').after('<div id="generic-vote-success"></div>');
-                   $('#generic-vote-success').text(Drupal.settings.Maxim.general_profile_data.vote_success_text);
+                   msg = Drupal.settings.Maxim.general_profile_data.vote_success_text.replace('\u003C', '<');
+                   msg = msg.replace('&lt;', '<');
+                   $('#generic-vote-success').html(msg);
                  });
                }
             },
