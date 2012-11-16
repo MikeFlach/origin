af8d1b3497804722c29fbcd7cb925d499b3afcb9
Henry Wan <hwan@maxim.com>
2012-11-16 17:26:15 -0500

    Hottie of the day - add 12 hour caching

--- docroot/sites/default/themes/maxim_base/templates/views-view--widget-hottie-day.tpl.php
+++ docroot/sites/default/themes/maxim_base/templates/views-view--widget-hottie-day.tpl.php
@@ -8,15 +8,15 @@ $widget_rid = rand(10,10000);
 $widget_ga = widget_build_ga();
 $widget_html = <<<WIDGETHTMLBLOCK
 <link rel="stylesheet" href="http://cdn2.maxim.com/maxim/sites/default/libraries/widget/widget.css" media="all" />
-<div id="mxm_widget_hottie_{$rid}" class="mxm-widget-hottie">
+<div id="mxm_widget_hottie_{$widget_rid}" class="mxm-widget-hottie">
   <div class="mxm-widget-head">{$header}</div>
   <div class="mxm-widget-body">{$rows}</div>
   <div class="mxm-widget-foot">{$footer}<div style="clear:both;font-size:1px;">&nbsp;</div></div>
 </div>
 WIDGETHTMLBLOCK;
 $widget_jsblock = <<<WIDGETJSBLOCK
-  var mxm_widget_hottie_{$rid}=document.getElementById("mxm_widget_hottie_{$rid}");
-  if(mxm_widget_hottie_{$rid}.offsetWidth < 210){mxm_widget_{$rid}.className+=" mxm-widget-hottie-sm";}
+  var mxm_widget_hottie_{$widget_rid}=document.getElementById("mxm_widget_hottie_{$widget_rid}");
+  if(mxm_widget_hottie_{$widget_rid}.offsetWidth < 210){mxm_widget_{$widget_rid}.className+=" mxm-widget-hottie-sm";}
   document.write('<img src="{$widget_ga}' + document.URL +'" />');
 WIDGETJSBLOCK;
 
