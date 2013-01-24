736c514d685e82e9104d9e099bf035eecd7fb709
Henry Wan <hwan@maxim.com>
2013-01-24 17:05:00 -0500

    Add freewheel feed

---
+++ docroot/feeds/freewheel/ingest.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Ingest response
+ */
+
+$filedir = $_SERVER['DOCUMENT_ROOT'] . '/sites/default/files/ads/freewheel_ingest_' . date('YmdHis') . '_';
+
+$str = '';
+foreach($_SERVER as $key=>$val) {
+  $str .= $key . ': ' . $val . "\n";
+}
+
+file_put_contents($filedir . 'servervars.txt', $str);
+
+try{
+  foreach($_FILES as $key=>$val) {
+    $filename = $filedir . $_FILES[$key]['name'];
+    $tmpname = $_FILES[$key]['tmp_name'];
+    move_uploaded_file ($tmpname, $filename);
+  }
+} catch(Exception $e) {
+
+}
