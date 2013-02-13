d4acaf5c846896daf56f5907c822977a4a164c7b
Henry Wan <hwan@maxim.com>
2013-02-13 11:41:18 -0500

    HTH map - increase memory to 256M

--- docroot/sites/default/settings.php
+++ docroot/sites/default/settings.php
@@ -38,10 +38,10 @@ switch ($_SERVER['HTTP_HOST']){
       ini_set('memory_limit', '256M');
     } else if (preg_match('/node\/([0-9])+\/edit/', $_GET['q']) === 1){
       ini_set('memory_limit', '192M');
-    } else if (strpos($_GET['q'], 'hometown-hotties') === 0) {
-      ini_set('memory_limit', '192M');
     } else if (strpos($_GET['q'], 'hth-map') > 0) {
       ini_set('memory_limit', '256M');
+    } else if (strpos($_GET['q'], 'hometown-hotties') === 0) {
+      ini_set('memory_limit', '192M');
     }
     break;
   case 'maxim.com':
