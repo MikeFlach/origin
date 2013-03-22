8af15aa8268319a92edfb9a283cd046f7ec38ea1
Michael Flach <mflach@maxim.com>
2013-03-22 16:56:31 -0400

    added reveal-admin and reveal view displays to hot100. also, ebnabled flag & flag_actions module

---
+++ docroot/sites/default/themes/maxim_base/templates/views-view-fields--hot-100--hot100-reveal.tpl.php
@@ -0,0 +1,49 @@
+<?php
+
+/**
+ * @file
+ * Default simple view template to all the fields as a row.
+ *
+ * - $view: The view in use.
+ * - $fields: an array of $field objects. Each one contains:
+ *   - $field->content: The output of the field.
+ *   - $field->raw: The raw data for the field, if it exists. This is NOT output safe.
+ *   - $field->class: The safe class id to use.
+ *   - $field->handler: The Views field handler object controlling this field. Do not use
+ *     var_export to dump this object, as it can't handle the recursion.
+ *   - $field->inline: Whether or not the field should be inline.
+ *   - $field->inline_html: either div or span based on the above flag.
+ *   - $field->wrapper_prefix: A complete wrapper containing the inline_html to use.
+ *   - $field->wrapper_suffix: The closing tag for the wrapper.
+ *   - $field->separator: an optional separator that may appear before a field.
+ *   - $field->label: The wrap label text to use.
+ *   - $field->label_html: The full HTML of the label to use including
+ *     configured element type.
+ * - $row: The raw result object from the query, with all data it fetched.
+ *
+ * @ingroup views_templates
+ */
+?>
+<?php 
+  $place_holder_image = 'http://cdn2.maxim.com/maxim/sites/default/files/coming_soon_thumb.jpg';
+  $place_holder_link = 'http://edit.maxim.com/girls-of-maxim/reveal-celebrity-profile';
+  $place_holder_image_html = '<div class="field-content"><a href="'.$place_holder_link.'"><img src="'.$place_holder_image.'" style="width:140px; height:100px"></a></div>';
+  $place_holder_name_html = '<div class="field-content"><a href="'.$place_holder_link.'">Not Yet!</a></div>';
+  
+  $flag = flag_get_flag('hot100_reveal');
+  if ($flag->is_flagged($fields['field_main_image']->raw)) {
+    $fields['field_main_image']->content = $place_holder_image_html;
+    $fields['field_hot_100_display_name']->content = $place_holder_name_html;
+  }
+
+  foreach ($fields as $id => $field):
+  ?>
+  <?php if (!empty($field->separator)): ?>
+    <?php print $field->separator; ?>
+  <?php endif; ?>
+
+  <?php print $field->wrapper_prefix; ?>
+    <?php print $field->label_html; ?>
+    <?php print $field->content; ?>
+  <?php print $field->wrapper_suffix; ?>
+<?php endforeach; ?>
