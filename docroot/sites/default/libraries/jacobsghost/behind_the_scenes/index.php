abc35583899a1beee92536f2e0cbface1911c602
Chris Gadzinski <cgadzinski@maxim.com>
2013-05-06 15:06:56 -0400

    New Files from QG added for testing

--- docroot/sites/default/libraries/jacobsghost/behind_the_scenes/index.php
+++ docroot/sites/default/libraries/jacobsghost/behind_the_scenes/index.php
@@ -8,7 +8,7 @@
 <!--[if (gt IEMobile 7)|!(IEMobile)]><!--> <html class="no-js" > <!--<![endif]-->
 <head>
   <meta charset="utf-8">
-  <title>Bloody_Ghost</title>
+  <title>Behind_The_Scenes</title>
   <meta property="og:restrictions:content" content="alcohol"/>
   <meta name="description" content=""/>
   <meta name="keywords" content=""/>
@@ -37,17 +37,18 @@ $(document).on("submit", function(){
 </head>
 <body data-role="body" >
 	<div style="display:block;width:100%;max-width:640px !important;margin:0px auto;">
-	<div data-role="page" id="bloody_ghost47440">
+	<div data-role="page" id="behind_the_scenes47848">
 		
 				<div data-role="header">
 
 				<a href="#" class="ui-btn-left" data-direction="reverse" data-icon="arrow-l" onclick="history.back();">Back</a>
-				<h1>Bloody_Ghost</h1>
+				<h1>Behind_The_Scenes</h1>
 				<a href="../jacobs_storm/index.php" data-icon="home" data-iconpos="notext" data-direction="reverse" class="ui-btn-right jqm-home">Home</a>
 				</div>
 			
-		<ul class="column ui-sortable ui-droppable" style="visibility: visible;"><li rel="logo" class="widget widget-type-logo box-logo " id="widget1" ><div class="widget-content"><div class="logo"><a href="/"><img src="../images/JACOBS_BLOODY_02_1.png" alt=""/></a></div></div></li>
+		<ul class="column ui-sortable ui-droppable" style="visibility: visible;"><li rel="logo" class="widget widget-type-logo box-logo " id="widget1" ><div class="widget-content"><div class="logo"><a href="../jacobs_storm/index.php"><img src="../images/JACOBS_BLOODY_02_1.png" alt=""/></a></div></div></li>
 <li rel="code" class="widget widget-type-code box-code " id="widget2" ><div class="widget-content">
+
 <style>
 #BCLcontainingBlock {
   width: 100%;
@@ -79,14 +80,12 @@ $(document).on("submit", function(){
 }
 </style>
 
-
-
 <div id="BCLcontainingBlock">
    <div class="BCLvideoWrapper">
 <div style="display:none;width:100%; height:270px;"></div>
 
 <script language="JavaScript" type="text/javascript" src="http://admin.brightcove.com/js/BrightcoveExperiences.js"></script>
-<object id="myExperience2318790764001" class="BrightcoveExperience">
+<object id="myExperience2318784908001" class="BrightcoveExperience">
   <param name="bgcolor" value="#000000" />
   <param name="width" value="100%" />
   <param name="height" value="100%" />
@@ -98,24 +97,24 @@ $(document).on("submit", function(){
   <param name="includeAPI" value="true" />
   <param name="templateLoadHandler" value="onTemplateLoaded" />
   <param name="templateReadyHandler" value="onTemplateReady" />
-  <param name="@videoPlayer" value="2318790764001" />
+  <param name="@videoPlayer" value="2318784908001" />
 </object>
 <script type="text/javascript">brightcove.createExperiences();</script>
 </div>
 </div>
 
 <!-- End of Brightcove Player --></div></li>
-<li rel="image" class="widget widget-type-image box-image " id="widget3" ><div class="widget-content"><div class="image"><img src="../images/JACOBS_BLOODY_05.png" alt=""/></div></div></li>
-<li rel="icon_navigation" class="widget widget-type-icon_navigation box-icon_navigation " id="widget4" ><div class="widget-content"><div class="image"><img class="icon_nav_img imgmap201341913543" src="../images/JACOBS_BLOODY_06.png" usemap="#imgmap201341913543"  alt=""/><map id="imgmap201341913543" name="imgmap201341913543"><area shape="rect" alt="" title="" coords="71,1,182,51" href="http://www.facebook.com/sharer.php?s=100
+<li rel="image" class="widget widget-type-image box-image " id="widget3" ><div class="widget-content"><div class="image"><img src="../images/JACOBS_STORM_05_6.png" alt=""/></div></div></li>
+<li rel="icon_navigation" class="widget widget-type-icon_navigation box-icon_navigation " id="widget4" ><div class="widget-content"><div class="image"><img class="icon_nav_img imgmap201341913314" src="../images/JACOBS_STORM_06_3.png" usemap="#imgmap201341913314"  alt=""/><map id="imgmap201341913314" name="imgmap201341913314"><area shape="rect" alt="" title="" coords="71,2,186,50" href="http://www.facebook.com/sharer.php?s=100
 &p[url]=http://maxim.com/jacobsghost
 &p[images][0]=http://cdn2.maxim.com/maxim/sites/default/files/jacobsshare.png
 &p[title]=Try these new Jacob's Ghost recipes!
-&p[summary]=Aged for one year, Jacob's Ghost is a smooth and flavorful white whiskey that can be enjoyed straight up or mixed like a clear spirit. Drink it any damn way you please." target="_blank" /><area shape="rect" alt="" title="" coords="71,57,182,107" href="https://www.twitter.com" target="" /><area shape="rect" alt="" title="" coords="188,2,318,108" href="http://www.jimbeam.com/jacobs-ghost" target="" /></map><script>$(document).ready(function() {
-		$(".imgmap201341913543").mapster({clickNavigate:true, fillOpacity: 0,scaleMap:true});
+&p[summary]=Aged for one year, Jacob's Ghost is a smooth and flavorful white whiskey that can be enjoyed straight up or mixed like a clear spirit. Drink it any damn way you please." target="_blank" /><area shape="rect" alt="" title="" coords="72,57,185,105" href="https://www.twitter.com" target="" /><area shape="rect" alt="" title="" coords="193,4,312,111" href="http://www.jimbeam.com/jacobs-ghost" target="" /></map><script>$(document).ready(function() {
+		$(".imgmap201341913314").mapster({clickNavigate:true, fillOpacity: 0,scaleMap:true});
 		
 		$(window).resize(function() {
 
-			var screenImage = $(".imgmap201341913543");
+			var screenImage = $(".imgmap201341913314");
 			var theImage = new Image();
 			theImage.src = screenImage.attr("src");
 			var imageWidth = theImage.width;
@@ -125,22 +124,22 @@ $(document).on("submit", function(){
 			{
 				if(imageWidth > 640)
 				{
-					$(".imgmap201341913543").mapster("resize",640);
+					$(".imgmap201341913314").mapster("resize",640);
 				}
 				else
 				{
-					$(".imgmap201341913543").mapster("resize",parseInt(imageWidth));
+					$(".imgmap201341913314").mapster("resize",parseInt(imageWidth));
 				}
 			}
 			else
 			{
-				$(".imgmap201341913543").mapster("resize",parseInt(winWidth));
+				$(".imgmap201341913314").mapster("resize",parseInt(winWidth));
 			}
 		});
 
-		$(".imgmap201341913543").load(function() {
+		$(".imgmap201341913314").load(function() {
 
-			var screenImage = $(".imgmap201341913543");
+			var screenImage = $(".imgmap201341913314");
 			var theImage = new Image();
 			theImage.src = screenImage.attr("src");
 			var imageWidth = theImage.width;
@@ -149,16 +148,16 @@ $(document).on("submit", function(){
 			{
 				if(imageWidth > 640)
 				{
-					$(".imgmap201341913543").mapster("resize",640);
+					$(".imgmap201341913314").mapster("resize",640);
 				}
 				else
 				{
-					$(".imgmap201341913543").mapster("resize",parseInt(imageWidth));
+					$(".imgmap201341913314").mapster("resize",parseInt(imageWidth));
 				}
 			}
 			else
 			{
-				$(".imgmap201341913543").mapster("resize",parseInt(winWidth));
+				$(".imgmap201341913314").mapster("resize",parseInt(winWidth));
 			}
 		});	
 		});</script></div></div></li>
@@ -219,8 +218,7 @@ a {text-decoration:none;}
 </style>
 <div style="text-align:center"></div><div style="text-align:center;color:#808080;font-size:x-small"><div style="text-align:center"><div><a style="font-weight:bold;color:#808080;font-size:xx-small" href="http://www.jimbeam.com/contact-us">Contact Us</a><span style="font-weight:bold;font-size:xx-small;color:#999999"> &nbsp; |  &nbsp; </span><a style="font-weight:bold;color:#808080;font-size:xx-small" href="http://www.jimbeam.com/privacy">Privacy Policy</a><span style="font-weight:bold;font-size:xx-small;color:#999999"> &nbsp; | &nbsp; </span><a style="font-weight:bold;color:#808080;font-size:xx-small" href="http://www.jimbeam.com/terms">Terms</a><br></div></div><br><span style="font-size:xx-small">©  2013 Beam Global Spirits and Wine, Inc. Deerfield, IL  <br>Jim Beam Brands Co. | 510 Lake Cook Rd. | Deerfield, IL  60015-4964. <br>All trademarks are property of their   respective owners.</span><br><br></div>  </div></div></li>
 </ul></div></div>
-	 
-   <script>    
+	<script>    
       var BCL = {};
       BCL.currentPlayerWidth = 1;
       var $BCLbodyContent = $('#BCLbodyContent');
