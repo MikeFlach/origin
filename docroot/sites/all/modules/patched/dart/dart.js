c899bd953602f808764ca2205c66d958885d1878
Henry Wan <hwan@maxim.com>
2013-08-07 10:18:30 -0400

    Rollback dart module to 7.x-2.5

--- docroot/sites/all/modules/patched/dart/dart.js
+++ docroot/sites/all/modules/patched/dart/dart.js
@@ -17,7 +17,7 @@ Drupal.DART.settings = {
  */
 Drupal.DART.tag = function(tag) {
   tag = typeof(tag) == 'string' ? eval('(' + tag + ')') : tag;
-
+console.log(tag);
   var tagname = tag.settings.options.method == 'adj' ? 'script' : 'iframe';
   var options = tag.settings.options.method == 'adj' ? 'type="text/javascript"' : 'frameborder="0" scrolling="no" width="' + tag.sz.split("x")[0] + '" height="' + tag.sz.split("x")[1] + '"';
 
