cfbfc243157722406404de3869bacd2b7992d446
Michael Flach <mflach@maxim.com>
2013-10-28 12:00:11 -0400

    turned on voting for hth

--- docroot/sites/default/libraries/php/hth_vote/set_uuid.php
+++ docroot/sites/default/libraries/php/hth_vote/set_uuid.php
@@ -3,8 +3,8 @@
   var uid = getCookie('maxim_uuid');
   var debug = false;
   var cacheBuster = Math.floor(Math.random()*1000000000);
-  // var isActive = (parent.Drupal.settings.Maxim.hottie.contest_status == 'active') ? true : false;
-  var isActive = false;
+  var isActive = (parent.Drupal.settings.Maxim.hottie.contest_status == 'active') ? true : false;
+  //var isActive = false;
 
   processVote = function(responseText) {
     objResponse = JSON.parse(responseText);
