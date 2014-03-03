b5b10eef446240c50c1188764eab8fc303dbb3fd
Henry Wan <hwan@maxim.com>
2014-03-03 14:01:02 -0500

    RSS - full content feed fix

--- docroot/sites/default/themes/maxim_base/templates/views-view-row-rss--full-content.tpl.php
+++ docroot/sites/default/themes/maxim_base/templates/views-view-row-rss--full-content.tpl.php
@@ -11,11 +11,12 @@
     <title><?php print $title; ?></title>
     <link><?php print $link; ?></link>
     <guid><?php print $link; ?></guid>
-    <description><?php print $description; ?></description>
-    <content:encoded>
-    <![CDATA[
-    <?php print $node->body[und][0][safe_value]; ?>
-    ]]>
-    </content:encoded>
+    <description><?php print $node->body[und][0][safe_value]; ?></description>
+    <?php
+    $item_elements = ereg_replace('<dc:creator>.*</dc:creator>', '', $item_elements);  // removes author
+    $item_elements = ereg_replace('<dc:creator />', '', $item_elements);  // removes author
+    $item_elements = ereg_replace('<guid.*</guid>', '', $item_elements);  // removes guid
+    ?>
+    <dc:creator><?php print $author_name ?></dc:creator>
     <?php print $item_elements; ?>
   </item>
